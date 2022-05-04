152a9b9666c0fdd9b37f4cab75d0d191ec5a951a
gentre <gthomse@calstatela.edu>
2022-05-04 02:21:12 -0700

    Renamed to exercises, tweaked layout, added rules, fixed a typo in the main css file.

--- js/exercises.js
+++ js/exercises.js
@@ -1,11 +1,3 @@
-/*!
-* Start Bootstrap - Business Frontpage v5.0.7 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
 /* Define card array for "carousel" */
 $(document).ready(function(){
   const cards=[
@@ -26,15 +18,15 @@ $(document).ready(function(){
     {cardHead: 'Biles',cardImage: 'assets/img/cards/2_of_diamonds.png'},
     {cardHead: 'McElroy',cardImage: 'assets/img/cards/3_of_diamonds.png'},
     {cardHead: 'T. Lee',cardImage: 'assets/img/cards/4_of_diamonds.png'},
-    {cardHead: 'Ryan? Reynolds',cardImage: 'assets/img/cards/5_of_diamonds.png'},
+    {cardHead: 'Ryan Reynolds',cardImage: 'assets/img/cards/5_of_diamonds.png'},
     {cardHead: 'Xia Ling',cardImage: 'assets/img/cards/6_of_diamonds.png'},
     {cardHead: 'Marty',cardImage: 'assets/img/cards/7_of_diamonds.png'},
     {cardHead: 'No Name Assigned',cardImage: 'assets/img/cards/8_of_diamonds.png'},
     {cardHead: 'Felix',cardImage: 'assets/img/cards/9_of_diamonds.png'},
     {cardHead: 'No Name Assigned',cardImage: 'assets/img/cards/10_of_diamonds.png'},
-    {cardHead: 'Gavin Newsome?',cardImage: 'assets/img/cards/jack_of_diamonds.png'},
-    {cardHead: 'Ms. Misk',cardImage: 'assets/img/cards/queen_of_diamonds.png'},
-    {cardHead: 'Elon Musk?',cardImage: 'assets/img/cards/king_of_diamonds.png'},
+    {cardHead: 'Gavin Newsome',cardImage: 'assets/img/cards/jack_of_diamonds.png'},
+    {cardHead: 'Mrs. Musk',cardImage: 'assets/img/cards/queen_of_diamonds.png'},
+    {cardHead: 'Elon Musk',cardImage: 'assets/img/cards/king_of_diamonds.png'},
     {cardHead: 'Shaquille Oneal',cardImage: 'assets/img/cards/ace_of_hearts.png'},
     {cardHead: 'Zhang',cardImage: 'assets/img/cards/2_of_hearts.png'},
     {cardHead: 'P. Barker',cardImage: 'assets/img/cards/3_of_hearts.png'},
@@ -56,7 +48,7 @@ $(document).ready(function(){
     {cardHead: 'E. Watson',cardImage: 'assets/img/cards/6_of_spades.png'},
     {cardHead: 'Jimi Hendrix',cardImage: 'assets/img/cards/7_of_spades.png'},
     {cardHead: 'Cady King',cardImage: 'assets/img/cards/8_of_spades.png'},
-    {cardHead: 'Blues?',cardImage: 'assets/img/cards/9_of_spades.png'},
+    {cardHead: 'Blues',cardImage: 'assets/img/cards/9_of_spades.png'},
     {cardHead: 'Tiffany Chen',cardImage: 'assets/img/cards/10_of_spades.png'},
     {cardHead: 'Jake',cardImage: 'assets/img/cards/jack_of_spades.png'},
     {cardHead: 'Brianna',cardImage: 'assets/img/cards/queen_of_spades.png'},
@@ -67,28 +59,32 @@ const cardContainer=$('#cardCarousel'); // Identify the carousel container
 
 for(let i in cards) { // Loop through each of the objects in the array
   const cardTemplate=$('#cardTemplate').clone(); // Copy the template
+  const cardText=$(cardTemplate.find('h1'));
   cardTemplate.removeClass('active'); // Remove the active class as only the first should be active
   if(i == 0) {cardTemplate.addClass('active')}; // Add the active for the first record
   cardTemplate.removeClass('d-none'); // Unhide
   cardTemplate.find('img').prop('src', cards[i].cardImage); // Set the image
   cardTemplate.find('img').prop('title', cards[i].cardHead); // Set the alt
-  cardTemplate.find('h1').text(cards[i].cardHead); // Set the header
+  cardText.text(cards[i].cardHead); // Set the header
+//  if(i < 13 || i > 38) {cardText.addClass('red')}; // Thought this would have been a nice effect, but couldn't get it to work.
   cardContainer.append(cardTemplate); // Append to the container
+  
 }
 
 function hideNames() {
-  $('#showName').prop('checked', false);
-  $('.cardText').addClass('d-none');
+  $('#showName').prop('checked', false); // Uncheck showName
+  $('.cardText').addClass('d-none'); // Hide name header
   
   }
 
 function showNames() {
-  $('#hideName').prop('checked', false);
-  $('.cardText').removeClass('d-none');
+  $('#hideName').prop('checked', false); // Uncheck hideName
+  $('.cardText').removeClass('d-none'); // Show name header
     
     
   }
-  
+   
+  // Event handlers
   $('#hideName').on('click', hideNames);
   $('#showName').on('click', showNames);
 
