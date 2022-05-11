df13f92307bafc5c0b34637cbbdf029065d3245a
gentre <gthomse@calstatela.edu>
2022-05-10 20:44:47 -0700

    reimplemented customStyle.css, broadened and made uniform the navbar, added section containers to journey.html and exercises.html

--- js/journey.js
+++ js/journey.js
@@ -34,18 +34,33 @@ $(document).ready(function () {
     { locationTitle: "", locationNarrative: "" }
   ]
 
+  function getSuffix(i) {
+    const j = i % 10;
+    const  k = i % 100;
+    if (j == 1 && k != 11) {
+      return i + "st";
+    }
+    if (j == 2 && k != 12) {
+      return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  }
+
   const locationCarousel = $('#locationCarousel'); // Identify the carousel container
   for (let i in locations) { // Loop through each of the objects in the array
+    const locationNumber = getSuffix(i);
+    const locationHead = locationNumber + ': ' + locations[i].locationTitle;
     const locationTemplate = $('#locationTemplate').clone(); // Copy the template
     const imagePath = '/assets/img/journey/' + i + '.jpeg'
     if (i == 0) { locationTemplate.addClass('active') }; // Add the active for the first record
     locationTemplate.removeClass('d-none'); // Unhide
     locationTemplate.find('img').prop('src', imagePath); // Set the image
-    locationTemplate.find('h5').text(locations[i].locationTitle); // Set the header
+    locationTemplate.find('h5').text(locationHead); // Set the header
     locationTemplate.find('p').text(locations[i].locationNarrative); // Set the narrative
     locationCarousel.append(locationTemplate); // Append to the container
-
-//    locationCarousel.find('#locationTemplate').remove();
   }
 
 });
